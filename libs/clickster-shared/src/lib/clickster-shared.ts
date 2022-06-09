135621e76e6ebb4c2be823ba866801787cd2bd8a
David Miranda Grinstein <davidmirandagrinstein@gmail.com>
2022-06-09 12:35:15 -0700

    Update clickster-shared.ts

--- libs/clickster-shared/src/lib/clickster-shared.ts
+++ libs/clickster-shared/src/lib/clickster-shared.ts
@@ -1,3 +1,9 @@
 export function clicksterShared(): string {
   return 'clickster-shared2';
 }
+
+// testing static analysis
+export function clicksterShared2(): string {
+  return 'clickster-shared2';
+}
+
